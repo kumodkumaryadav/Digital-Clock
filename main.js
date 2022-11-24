5179b7b37770354d41f2b9f5202e426991c55916
Kumod Kr. Yadav <48380509+kumod353@users.noreply.github.com>
2022-11-24 14:37:15 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,13 +3,14 @@
      var h = d.getHours();
      var m = d.getMinutes();
      var s = d.getSeconds();
-     var session = "AM"
+     var session;
         h = h < 10 ? "0"+h : h;
         m = m < 10 ? "0"+m : m;
         s = s < 10 ? "0"+s : s;
      var time = h + " : " + m + " : " + s + " " + session;
      if (h>12) {
          h  = h - 12;
+      session="AM";
      }
 
      if(h>=12) {
